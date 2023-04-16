077e7d27f0bee905356457f86a6e6fc87f3292bf
spotts01 <Spotts7652@conestogac.on.ca>
2023-04-16 14:18:16 -0400

    added spinner class which times out after 1 second

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,45 @@
 console.log('hello world')
+
+
+const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+helloWorldBox.innerHTML = 'hello <b>world</b>'
+const spinnerBox = document.getElementById('spinner-box')
+
+$.ajax({
+    type: 'GET',
+    url: 'hello-world/',
+    success: function(reponse)
+    {
+        console.log('success', reponse)
+        helloWorldBox.textContent = reponse.text
+    },
+
+    error: function(error)
+    {
+        console.log('error', error)
+    }
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+
+    success: function (response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - <b>${el.body}</b> <br>
+                `
+            });
+        }, 1000)
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
