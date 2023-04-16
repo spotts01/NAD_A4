f49842818fbff440cdb9499567adba3b75a5b4eb
spotts01 <Spotts7652@conestogac.on.ca>
2023-04-16 00:12:48 -0400

    added posts to show posts on main page

--- a4/settings.py
+++ a4/settings.py
@@ -121,17 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [ 
-    BASE_DIR / 'static', 
-    BASE_DIR / 'posts'/'static',
-    BASE_DIR / 'profiles'/'static',
+    BASE_DIR /'static', 
+    BASE_DIR /'posts'/'static',
+    BASE_DIR /'profiles'/'static',
     ]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
--- a4/urls.py
+++ a4/urls.py
@@ -22,8 +22,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls', namespace='posts')),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
